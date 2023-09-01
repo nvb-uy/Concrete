d5b8ce5de5559074faecefd26220faf2af4c2a8f
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-31 21:13:45 -0300

    fix(array length)

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -1,4 +1,4 @@
-pub const MINECRAFT_WORDS: [&str; 2048] = [
+pub const MINECRAFT_WORDS: [&str; 201] = [
     "axe", "pick", "craft", "mine", "build",  "stone", "ore", "wood", "tree", "dirt",
     "sand", "grass", "water", "lava", "cave", "torch", "fire", "bow", "arrow", "sword", "mob",
     "cow", "sheep", "pig", "chicken", "wolf", "zombie", "creeper", "enderman", "skeleton", "spider",
